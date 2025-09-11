9ba923570949526787962767d000bd188b9ae7fc
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-09-11 19:15:09 +0300

    added functionality for file reading, basic still need to expand. also made chat mode the default

--- ask.py
+++ ask.py
@@ -1,4 +1,3 @@
-
 import sys
 from google import genai
 
@@ -10,57 +9,48 @@ def one_shot(prompt):
     for chunk in response:
         print(chunk.text, end='')
 
-        
+    print(' \n\n--------------------------------------------------------------------------------\n\n') 
 
-def chat_mode():
+def chat_mode(file=None):
     chat = client.chats.create(model='gemini-2.5-flash')
 
+    file_content = file.read() if file else ''
+    
     while True:
         prompt = input('\n(You): ')
 
         if prompt == 'exit':
             break
 
-        response = chat.send_message_stream(prompt)
-        
-        print('\n\nResponse: ')
+        response = chat.send_message_stream(prompt + file_content)
+        print(' \n\n--------------------------------------------------------------------------------\n\n')
+
+        print('Response: ')
         for chunk in response:
             print(chunk.text, end='')
-        print(' \n\n----------------------------------------\n\n')
-    
 
+        print(' \n\n--------------------------------------------------------------------------------\n\n')
 
 def main():
-
     import argparse
 
     parser = argparse.ArgumentParser(description="Custom CLI tool for llm usage")
 
-    parser.add_argument(
-        '--no-chat',
-        type='bool',
-        help='Use for prompting just one message instead of starting a full chat.'
-    )
-
-    parser.add_argument(
-        '--file',
-        type=argparse.FileType('r', encoding='utf-8'),
-        help='Path to a file that is to be given to the model'
-    )
+    parser.add_argument('--chat-mode', action='store_true', help='Use for prompting just one message instead of starting a full chat.')
+    parser.add_argument('--file', default='./samples/sample.txt', type=argparse.FileType('r', encoding='utf-8'), help='Path to a file that is to be given to the model')
 
     args = parser.parse_args()
 
-    # if args.no_chat:
-    #     prompt = " ".join(sys.argv[1:])
-    #     one_shot(prompt)
-
-    if not args.no_chat:
-        chat_mode()
+    if not args.chat_mode:  #set as default for now
+        print('Chat Mode')
+        chat_mode(file=args.file)
 
     else:
-        print('Useage: \nOne shot: askai <prompt>\nChat Mode: askai chat')
-        
+        print('One Run Mode')
+        one_shot(' '.join(sys.argv[2:]) if len(sys.argv) > 2 else '')
 
+        # print('Useage: \nOne shot: askai <prompt>\nChat Mode: askai chat')
+        
 if __name__ == "__main__":
     print('Starting Google AI Studio cli tool')
     client = genai.Client()
